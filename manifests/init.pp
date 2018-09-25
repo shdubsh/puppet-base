5ff12507430a1cd90be095e9c268688ac8d5c471
Cole White <cwhite@wikimedia.org>
2018-09-25 16:19:12 -0600

    hostgroups and servicegroups

--- manifests/init.pp
+++ manifests/init.pp
@@ -2,4 +2,13 @@ class base {
   # if ($role != 'unassigned') {
   #   include monitoring
   # }
+
+  monitoring::host { $::hostname:
+    group => "${role}_${datacenter}"
+  }
+
+  monitoring::service { 'ssh':
+    description   => 'SSH',
+    check_command => 'check_ssh',
+  }
 }
